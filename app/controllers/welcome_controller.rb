14cb77fbb6f990d4e3a80230b6cfc7c8cf65110c
Aaron <aaron.chong13@sps.nus.edu.sg>
2016-09-12 19:39:04 +0800

    Get user input and print output

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,19 @@
 class WelcomeController < ApplicationController
+
+  # Here's the description of the requirements:
+  # 1. On the main home page, the user can enter a space-separated string to be used as the input (e.g. "A B C D")
+  # 2. The platform should output the transformed version of the input string
+  # 3. The lookup table should be implemented in a database (Postgres)
+  # 4. The app should be deployable to Heroku (you can get a free account if you don't have one)
+
+  def index
+  end
+
+  def transform
+    input = params[:input]
+    # transform
+    @output = input.split(" ")
+    render :index
+  end
+
 end
